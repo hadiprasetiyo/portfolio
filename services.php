a47b0ca663df8b0be0a68e1d3d2bf5b7e217ea77
hadiprasetiyo <hadiiyok01@gmail.com>
2024-01-02 17:20:51 +0800

    feat(services): membaut halaman services untuk memberikan penjelasan keahlian saya

---
+++ services.php
@@ -0,0 +1,97 @@
+<section class="colorlib-services" data-section="services">
+  <div class="colorlib-narrow-content">
+    <div class="row">
+      <div
+        class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+        data-animate-effect="fadeInLeft"
+      >
+        <span class="heading-meta">What I do?</span>
+        <h2 class="colorlib-heading">Here are some of my expertise</h2>
+      </div>
+    </div>
+    <div class="row row-pt-md">
+      <div class="col-md-4 text-center animate-box">
+        <div class="services color-1">
+          <span class="icon">
+            <i class="icon-bulb"></i>
+          </span>
+          <div class="desc">
+            <h3>Innovative Ideas</h3>
+            <p>
+            Sebagai individu dengan keahlian dalam menciptakan ide-ide inovatif, saya memiliki kemampuan untuk berpikir kreatif dan melihat peluang di setiap tantangan.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 text-center animate-box">
+        <div class="services color-2">
+          <span class="icon">
+            <i class="icon-data"></i>
+          </span>
+          <div class="desc">
+            <h3>Database Management</h3>
+            <p>
+            Dalam bidang manajemen basis data, saya memiliki keahlian dalam merancang, mengelola, dan mengoptimalkan basis data.
+            Saya terampil dalam menggunakan sistem manajemen basis data (DBMS)
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 text-center animate-box">
+        <div class="services color-3">
+          <span class="icon">
+            <i class="icon-phone3"></i>
+          </span>
+          <div class="desc">
+            <h3>Android Developer</h3>
+            <p>
+            Sebagai pengembang Android, saya memiliki pengalaman yang solid dalam merancang dan mengimplementasikan aplikasi mobile untuk platform Android. 
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 text-center animate-box">
+        <div class="services color-4">
+          <span class="icon">
+            <i class="icon-layers2"></i>
+          </span>
+          <div class="desc">
+            <h3>UI/UX Design</h3>
+            <p>
+              Keahlian saya dalam UI/UX Design memungkinkan saya untuk menggabungkan fungsi dan estetika.
+              Saya memiliki pemahaman yang baik tentang perangkat lunak desain seperti Figma.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 text-center animate-box">
+        <div class="services color-5">
+          <span class="icon">
+            <i class="icon-data"></i>
+          </span>
+          <div class="desc">
+            <h3>Software</h3>
+            <p>
+            Saya terampil dalam menggunakan bahasa pemrograman untuk menciptakan solusi perangkat lunak yang efisien.
+            Saya juga memiliki kemampuan untuk mengidentifikasi dan memecahkan masalah.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 text-center animate-box">
+        <div class="services color-6">
+          <span class="icon">
+            <i class="fas fa-cogs"></i>
+          </span>
+          <div class="desc">
+            <h3>Rest API</h3>
+            <p>
+            Dalam pengembangan Rest API, saya memiliki kemampuan untuk merancang dan mengimplementasikan layanan web
+            yang efisien dan dapat diandalkan untuk mendukung aplikasi.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
