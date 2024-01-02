7c0e5d3da36027de7ad62d8a893e88994c8fcc37
hadiprasetiyo <hadiiyok01@gmail.com>
2024-01-02 17:24:32 +0800

    feat(education): menambahkan halaman education untuk memberikan informasi pengalaman pendidikan

---
+++ education.php
@@ -0,0 +1,115 @@
+<section class="colorlib-experience" data-section="experience">
+  <div class="colorlib-narrow-content">
+    <div class="row">
+      <div
+        class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+        data-animate-effect="fadeInLeft"
+      >
+        <span class="heading-meta">Education</span>
+        <h2 class="colorlib-heading animate-box">Education</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="timeline-centered">
+          <article
+            class="timeline-entry animate-box"
+            data-animate-effect="fadeInLeft"
+          >
+            <div class="timeline-entry-inner">
+              <div class="timeline-icon color-1">
+                <i class="icon-pen2"></i>
+              </div>
+
+              <div class="timeline-label">
+                <h2>
+                  <a href="#">Universitas Mulawarman</a> <span>2021 - Sekarang</span>
+                </h2>
+                <p>
+                  Universitas Mulawarman merupakan universitas terbaik di Kalimantan Timur.
+                  Saya mengambil jurusan Sistem Informasi, Fakultas Teknik.
+                  Di sana saya belajar banyak tentang teknologi, dan saya memutuskan untuk mengeksplorasi
+                  lebih jauh tentang Web Developer.
+                </p>
+              </div>
+            </div>
+          </article>
+
+          <article
+            class="timeline-entry animate-box"
+            data-animate-effect="fadeInRight"
+          >
+            <div class="timeline-entry-inner">
+              <div class="timeline-icon color-2">
+                <i class="icon-pen2"></i>
+              </div>
+              <div class="timeline-label">
+                <h2>
+                  <a href="#">SMAN 9 Samarinda</a>
+                  <span>2017 - 2020</span>
+                </h2>
+                <p>
+                  SMAN 9 merupakan salah satu SMA Negeri di Samarinda.
+                  Saya bersekolah di sini selama 3 tahun dan saya belajar lebih banyak tentang pengetahuan umum.
+                  Saya mengambil jurusan IPA. Pada saat itulah saya mulai tertarik pada teknologi.
+                </p>
+              </div>
+            </div>
+          </article>
+
+          <article
+            class="timeline-entry animate-box"
+            data-animate-effect="fadeInLeft"
+          >
+            <div class="timeline-entry-inner">
+              <div class="timeline-icon color-3">
+                <i class="icon-pen2"></i>
+              </div>
+              <div class="timeline-label">
+                <h2><a href="#">SMPN 13 Samarinda</a> <span>2015 - 2017</span></h2>
+                <p>
+                  SMPN 13 merupakan salah satu SMP Negeri di Samarinda.
+                  Saya bersekolah di sekolah ini selama 3 tahun dan saya belajar
+                  lebih banyak tentang pengetahuan umum.
+                  Saat itu saya mengikuti kegiatan ekstrakurikuler Pramuka dan Badminton.
+                </p>
+              </div>
+            </div>
+          </article>
+
+          <article
+            class="timeline-entry animate-box"
+            data-animate-effect="fadeInTop"
+          >
+            <div class="timeline-entry-inner">
+              <div class="timeline-icon color-4">
+                <i class="icon-pen2"></i>
+              </div>
+              <div class="timeline-label">
+                <h2>
+                  <a href="#">SDN 027 Samarinda</a> <span>2008 - 2014</span>
+                </h2>
+                <p>
+                  SDN 027 merupakan salah satu Sekolah Dasar Negeri di Samarinda.
+                  Saya bersekolah di sekolah ini selama 6 tahun dan banyak belajar di sini,
+                  mulai dari pelajaran umum.
+                </p>
+              </div>
+            </div>
+          </article>
+
+          
+
+          <article
+            class="timeline-entry begin animate-box"
+            data-animate-effect="fadeInBottom"
+          >
+            <div class="timeline-entry-inner">
+              <div class="timeline-icon color-none"></div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
