2d21265e15d1a2f1dfb457bf48227012e086ee0e
hadiprasetiyo <hadiiyok01@gmail.com>
2024-01-02 17:22:34 +0800

    feat(skills): menambahkan halaman skill untuk memberikan informasi persentase skill saya

---
+++ skills.php
@@ -0,0 +1,208 @@
+<section class="colorlib-skills" data-section="skills">
+  <div class="colorlib-narrow-content">
+    <div class="row">
+      <div
+        class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+        data-animate-effect="fadeInLeft"
+      >
+        <span class="heading-meta">My Specialty</span>
+        <h2 class="colorlib-heading animate-box">My Skills</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 animate-box" data-animate-effect="fadeInLeft">
+        <p style="font-size: 15px;">
+          Ini adalah persentase skill saya, dari beberapa bahasa pemrograman, framework, library dll. yang telah saya kuasai dan mempelajarinya.
+        </p>
+      </div>
+      <div class="col-md-6 animate-box" data-animate-effect="fadeInLeft">
+        <div class="progress-wrap">
+          <h3>HTML</h3>
+          <div class="progress">
+            <div
+              class="progress-bar color-1"
+              role="progressbar"
+              aria-valuenow="75"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style="width: 95%"
+            >
+              <span>95%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 animate-box" data-animate-effect="fadeInRight">
+        <div class="progress-wrap">
+          <h3>PHP</h3>
+          <div class="progress">
+            <div
+              class="progress-bar color-6"
+              role="progressbar"
+              aria-valuenow="80"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style="width: 90%"
+            >
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 animate-box" data-animate-effect="fadeInLeft">
+        <div class="progress-wrap">
+          <h3>CSS</h3>
+          <div class="progress">
+            <div
+              class="progress-bar color-3"
+              role="progressbar"
+              aria-valuenow="85"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style="width: 95%"
+            >
+              <span>95%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 animate-box" data-animate-effect="fadeInRight">
+        <div class="progress-wrap">
+          <h3>Python</h3>
+          <div class="progress">
+            <div
+              class="progress-bar color-2"
+              role="progressbar"
+              aria-valuenow="60"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style="width: 90%"
+            >
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 animate-box" data-animate-effect="fadeInLeft">
+        <div class="progress-wrap">
+          <h3>Javascript</h3>
+          <div class="progress">
+            <div
+              class="progress-bar color-5"
+              role="progressbar"
+              aria-valuenow="70"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style="width: 90%"
+            >
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 animate-box" data-animate-effect="fadeInRight">
+        <div class="progress-wrap">
+          <h3>JAVA</h3>
+          <div class="progress">
+            <div
+              class="progress-bar color-4"
+              role="progressbar"
+              aria-valuenow="90"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style="width: 90%"
+            >
+              <span>90%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 animate-box" data-animate-effect="fadeInLeft">
+        <div class="progress-wrap">
+          <h3>MySQL</h3>
+          <div class="progress">
+            <div
+              class="progress-bar color-7"
+              role="progressbar"
+              aria-valuenow="70"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style="width: 95%"
+            >
+              <span>95%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 animate-box" data-animate-effect="fadeInRight">
+        <div class="progress-wrap">
+          <h3>Figma</h3>
+          <div class="progress">
+            <div
+              class="progress-bar color-8"
+              role="progressbar"
+              aria-valuenow="80"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style="width: 95%"
+            >
+              <span>95%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 animate-box" data-animate-effect="fadeInRight">
+        <div class="progress-wrap">
+          <h3>Laravel</h3>
+          <div class="progress">
+            <div
+              class="progress-bar color-8"
+              role="progressbar"
+              aria-valuenow="80"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style="width: 95%"
+            >
+              <span>95%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 animate-box" data-animate-effect="fadeInRight">
+        <div class="progress-wrap">
+          <h3>Rest API</h3>
+          <div class="progress">
+            <div
+              class="progress-bar color-8"
+              role="progressbar"
+              aria-valuenow="80"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style="width: 95%"
+            >
+              <span>95%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 animate-box" data-animate-effect="fadeInRight">
+        <div class="progress-wrap">
+          <h3>Git & GitHub</h3>
+          <div class="progress">
+            <div
+              class="progress-bar color-8"
+              role="progressbar"
+              aria-valuenow="80"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style="width: 95%"
+            >
+              <span>95%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
