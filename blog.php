e3cd26616ae0d9736f143b91cb6b70a991f82f51
hadiprasetiyo <hadiiyok01@gmail.com>
2024-01-02 17:27:51 +0800

    feat(blog): menambahkan halaman blog untuk informasi entri blog yang telah saya buat

---
+++ blog.php
@@ -0,0 +1,50 @@
+<section class="colorlib-blog" data-section="blog">
+				<div class="colorlib-narrow-content">
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+							<span class="heading-meta">Read</span>
+							<h2 class="colorlib-heading">Recent Blog</h2>
+						</div>
+					</div>
+					<div class="row">
+
+					<div class="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInRight">
+							<div class="blog-entry">
+								<a href="https://medium.com/@hadiiyok" class="blog-img"><img src="images/blog1.png" style="width:500px; height:190px;" class="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com"></a>
+								<div class="desc">
+									<span><small>Mei 24, 2022 </small> | <small> UX Case Study </small> | <small> <i class="icon-bubble3"></i> 4</small></span>
+									<h3><a href="https://medium.com/@hadiiyok">Design Process | UIUX</a></h3>
+									<p>Design Thinking adalah pendekatan untuk memecahkan masalah yang berpusat pada pengguna.</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+							<div class="blog-entry">
+								<a href="https://sisteminformasiunmul.blogspot.com/" class="blog-img"><img src="images/blog2.png" style="width:500px; height:190px;" class="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com"></a>
+								<div class="desc">
+									<span><small>April 14, 2022 </small> | <small> My Blogspot </small> | <small> <i class="icon-bubble3"></i> 4</small></span>
+									<h3><a href="https://sisteminformasiunmul.blogspot.com/">Infomation linux</a></h3>
+									<p>Linux adalah keluarga sistem operasi mirip Unix bebas dan sumber terbuka yang didasarkan pada kernel Linux, yaitu sebuah kernel sistem operasi yang pertama kali dikembangkan oleh Linus Torvalds pada 1991.</p>
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+							<div class="blog-entry">
+								<a href="https://sisteminformasiunmul.blogspot.com/2022/04/penjelasan-mengenai-slax-linux.html" class="blog-img"><img src="images/blog3.png" style="width:500px; height:190px;" class="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com"></a>
+								<div class="desc">
+									<span><small>April 14, 2018 </small> | <small> Slax Linux </small> | <small> <i class="icon-bubble3"></i> 4</small></span>
+									<h3><a href="https://sisteminformasiunmul.blogspot.com/2022/04/penjelasan-mengenai-slax-linux.html">Penjelasan Slax Linux</a></h3>
+									<p>Slax adalah salah satu portabel terkecil dan sistem operasi Linux cepat dengan pendekatan modular dan desain luar biasa yang dibangun untuk dijalankan dari DVD atau thumb drive.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 animate-box">
+							<p><a href="#" class="btn btn-primary btn-lg btn-load-more">Load more <i class="icon-reload"></i></a></p>
+						</div>
+					</div>
+				</div>
+			</section>
