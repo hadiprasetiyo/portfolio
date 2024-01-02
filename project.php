4d67cbd6b1edf8fe12a895e651eb879758daf29c
hadiprasetiyo <hadiiyok01@gmail.com>
2024-01-02 17:26:43 +0800

    feat(project): menambahkan halaman project untuk informasi project yang telah saya buat

---
+++ project.php
@@ -0,0 +1,120 @@
+<section class="colorlib-work" data-section="work">
+  <div class="colorlib-narrow-content">
+    <div class="row">
+      <div
+        class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+        data-animate-effect="fadeInLeft"
+      >
+        <span class="heading-meta">My Project</span>
+        <h2 class="colorlib-heading animate-box">Project</h2>
+      </div>
+    </div>
+    <div
+      class="row row-bottom-padded-sm animate-box"
+      data-animate-effect="fadeInLeft"
+    >
+      <div class="col-md-12">
+        <p class="work-menu">
+          <span class="work-filter"><a href="#" class="filter active" data-filter="all">All</a></span>
+          <span class="work-filter"><a href="#" class="filter" data-filter=".web">Web Design </a></span>
+          <span class="work-filter"><a href="#" class="filter" data-filter=".uiux">UI/UX</a></span>
+        </p>
+      </div>
+    </div>
+    <div class="row work-item">
+      <!-- card 1 -->
+      <div class="col-md-6 animate-box web" data-animate-effect="fadeInLeft">
+        <div class="project" style="background-image: url(images/tugas4.png)">
+          <div class="desc">
+            <div class="con">
+              <h3><a href="work.html">Tugas 4</a></h3>
+              <span>CSS</span>
+              <p class="icon">
+                <span><a href="https://peserta39-fwd.sib5.nurulfikri.com/tugas4/" target="_blank"><i class="icon-eye"></i> Visite</a></span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- card 2 -->
+      <div class="col-md-6 animate-box uiux" data-animate-effect="fadeInRight">
+        <div class="project" style="background-image: url(images/pa-uiux.png)">
+          <div class="desc">
+            <div class="con">
+            <h3><a href="work.html">Project Akhir</a></h3>
+              <span>UI/UX</span>
+              <p class="icon">
+                <span><a href="https://www.figma.com/file/0KvKmB1JfHvfXXFOqa7xeh/Delicious-Threats?type=design&node-id=0%3A1&mode=design&t=c4bUonHixLsDE881-1" target="_blank"><i class="icon-eye"></i> Visite</a></span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- card 3 -->
+      <div class="col-md-6 animate-box web" data-animate-effect="fadeInTop">
+        <div class="project" style="background-image: url(images/adminpage.png)">
+          <div class="desc">
+            <div class="con">
+              <h3><a href="work.html">Admin Page</a></h3>
+              <span>Website</span>
+              <p class="icon">
+                <span><a href="https://peserta39-fwd.sib5.nurulfikri.com/adminpage/" target="_blank"><i class="icon-eye"></i> Visite</a></span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- card 4 -->
+      <div class="col-md-6 animate-box web" data-animate-effect="fadeInBottom">
+        <div class="project" style="background-image: url(images/landingpage.png)">
+          <div class="desc">
+            <div class="con">
+              <h3><a href="work.html">Landing Page</a></h3>
+              <span>Website</span>
+              <p class="icon">
+                <span><a href="https://peserta39-fwd.sib5.nurulfikri.com/landingpage/" target="_blank"><i class="icon-eye"></i> Visite</a></span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- card 5 -->
+      <div class="col-md-6 animate-box uiux" data-animate-effect="fadeInLeft">
+        <div class="project" style="background-image: url(images/tugasUIUX.png)">
+          <div class="desc">
+            <div class="con">
+              <h3><a href="work.html">Tugas UIUX</a></h3>
+              <span>Figma</span>
+              <p class="icon">
+                <span><a href="https://www.figma.com/file/WGczflWTwNrTNUROm17w9l/Untitled?type=design&node-id=0%3A1&mode=design&t=SfR6nsVDray5IwGm-1" target="_blank"><i class="icon-eye"></i> Visite</a></span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- card 6 -->
+      <div class="col-md-6 animate-box web" data-animate-effect="fadeInRight">
+        <div class="project" style="background-image: url(images/tugas5.png)">
+          <div class="desc">
+            <div class="con">
+              <h3><a href="work.html">Tugas 5</a></h3>
+              <span>Bootstrap</span>
+              <p class="icon">
+                <span><a href="https://peserta39-fwd.sib5.nurulfikri.com/tugas5/" target="_blank"><i class="icon-eye"></i> Visite</a></span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row">
+      <div class="col-md-12 animate-box">
+        <p class="btn btn-primary btn-lg btn-load-more">Scroll for Load more <i class="icon-reload"></i></p>
+      </div>
+    </div>
+
+  </div>
+</section>
+
