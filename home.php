7598ae63c61cf24b6315c5597c73863865afb4ac
hadiprasetiyo <hadiiyok01@gmail.com>
2024-01-02 17:15:43 +0800

    feat: membuat halaman home sebagai profile homepage dan juga terdapat download file CV

---
+++ home.php
@@ -0,0 +1,54 @@
+<section id="colorlib-hero" class="js-fullheight" data-section="home">
+  <div class="flexslider js-fullheight">
+    <ul class="slides">
+      <li style="background-image: url(images/background1.png)">
+        <div class="overlay"></div>
+        <div class="container-fluid">
+          <div class="row">
+            <div
+              class="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text"
+            >
+              <div class="slider-text-inner js-fullheight">
+                <div class="desc">
+                  <h1>Hi! <br />I'm Hadi Prasetiyo</h1>
+                  <h2>
+                    Wellcome My profile Homepage
+                  </h2>
+                  <p>
+                    <a href="cv/cvHadiPrasetiyo.pdf" class="btn btn-primary btn-learn"
+                      >Download CV <i class="icon-download4"></i
+                    ></a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+      <li style="background-image: url(images/background2.png)">
+        <div class="overlay"></div>
+        <div class="container-fluid">
+          <div class="row">
+            <div
+              class="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text"
+            >
+              <div class="slider-text-inner">
+                <div class="desc">
+                  <h1>I am <br />Programmer</h1>
+                  <h2>
+                    Fullstack Web Developer
+                  </h2>
+                  <p>
+                    <a href="?hal=project" class="btn btn-primary btn-learn"
+                      >View Portfolio <i class="icon-briefcase3"></i
+                    ></a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
