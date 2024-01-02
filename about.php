57169348c28768d8e5c4253c262b7a093cf3c2bd
hadiprasetiyo <hadiiyok01@gmail.com>
2024-01-02 17:17:55 +0800

    feat: membuat halaman about untuk memberikan informasi pribadi tentang diri saya secara rinci

---
+++ about.php
@@ -0,0 +1,65 @@
+<section class="colorlib-about" data-section="about">
+  <div class="colorlib-narrow-content">
+    <div class="row">
+      <div class="col-md-12">
+        <div
+          class="row row-bottom-padded-sm animate-box"
+          data-animate-effect="fadeInLeft"
+        >
+          <div class="col-md-12">
+            <div class="about-desc">
+              <span class="heading-meta">About Us</span>
+              <h2 class="colorlib-heading">Who Am I?</h2>
+              <p style="font-size: 15px;">
+                <strong>Hi!</strong> Saya <strong>Hadi Prasetiyo</strong> mahasiswa Universitas Mulawarman, Fakultas Teknik, Program Studi Sistem Informasi,
+                Semester 6. Usia saya 21 Tahun. saya berasal dari Samarinda, Kalimantan Timur. Saya memiliki cita cita menjadi
+                programmer yang sukses. Saya memiliki motivasi yang tinggi untuk terus belajar dan mengembangkan diri di bidang teknologi.
+                Saya akan terus berusaha belajar untuk mendapatkan ilmu yang lebih luas.
+              </p>
+              <p style="font-size: 15px;">
+                Saya ingin menjadi programmer yang sukses dan bekerja diperusahaan ternama. Saya juga ingin membuat website profesional yang dapat dipergunakan untuk mempermudah pekerjaan.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 animate-box" data-animate-effect="fadeInLeft">
+            <div class="services color-1">
+              <span class="icon2"><i class="icon-bulb"></i></span>
+              <h3>UI/UX Design</h3>
+            </div>
+          </div>
+          <div class="col-md-3 animate-box" data-animate-effect="fadeInRight">
+            <div class="services color-2">
+              <span class="icon2"><i class="icon-globe-outline"></i></span>
+              <h3>Web Design</h3>
+            </div>
+          </div>
+          <div class="col-md-3 animate-box" data-animate-effect="fadeInTop">
+            <div class="services color-3">
+              <span class="icon2"><i class="icon-data"></i></span>
+              <h3>Database Management</h3>
+            </div>
+          </div>
+          <div class="col-md-3 animate-box" data-animate-effect="fadeInBottom">
+            <div class="services color-4">
+              <span class="icon2"><i class="fas fa-cogs"></i></span>
+              <h3>Rest API</h3>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 animate-box" data-animate-effect="fadeInLeft">
+            <div class="hire">
+              <p style="font-size: 17px;">
+                <em>Dalam setiap baris kode, terdapat jejak pemikiran, logika, dan dedikasi.<br>Kode adalah bahasa bisu yang berbicara dengan mesin.</em>
+              </p>
+              <a href="#" class="btn-hire">- hadiiyok</a>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</section>
