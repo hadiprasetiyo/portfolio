654744bd4c05d6db82efe201fb4d613bdd7fbe9e
hadiprasetiyo <hadiiyok01@gmail.com>
2024-01-02 17:28:59 +0800

    feat(contact): membuat halaman contact untuk informasi contact person saya

---
+++ contact.php
@@ -0,0 +1,98 @@
+<section class="colorlib-contact" data-section="contact">
+  <div class="colorlib-narrow-content">
+    <div class="row">
+      <div
+        class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+        data-animate-effect="fadeInLeft"
+      >
+        <span class="heading-meta">Get in Touch</span>
+        <h2 class="colorlib-heading">Contact</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-5">
+        <div
+          class="colorlib-feature colorlib-feature-sm animate-box"
+          data-animate-effect="fadeInLeft"
+        >
+          <div class="colorlib-icon">
+            <i class="icon-globe-outline"></i>
+          </div>
+          <div class="colorlib-text">
+            <p><a href="https://peserta39-fwd.sib5.nurulfikri.com/">hadiiyok@nurulfikri.com</a></p>
+          </div>
+        </div>
+
+        <div
+          class="colorlib-feature colorlib-feature-sm animate-box"
+          data-animate-effect="fadeInLeft"
+        >
+          <div class="colorlib-icon">
+            <i class="icon-map"></i>
+          </div>
+          <div class="colorlib-text">
+            <p>Samarinda, Kalimantan Timur, Indonesia. 75118</p>
+          </div>
+        </div>
+
+        <div
+          class="colorlib-feature colorlib-feature-sm animate-box"
+          data-animate-effect="fadeInLeft"
+        >
+          <div class="colorlib-icon">
+            <i class="icon-phone"></i>
+          </div>
+          <div class="colorlib-text">
+            <p><a href="whatsapp://send?text=Hello&phone=+6285711228592" target="_blank">+62 857-1122-8592</a></p>
+          </div>
+        </div>
+
+        <div class="d-flex pt-2 animate-box gap-5" data-animate-effect="fadeInLeft">
+          
+        <p style="font-size: 17px"><b>Follow Me :</b></p>
+          <a class="btn btn-square btn-primary me-2 mr-5" href="https://www.instagram.com/hadiiprasetiyo/" target="_blank"><i class="fab fa-instagram"></i></a>
+          <a class="btn btn-square btn-primary me-2 mr-5" href="https://discordapp.com/users/hadiiyok/" target="_blank"><i class="fab fa-discord"></i></a>
+          <a class="btn btn-square btn-primary me-2 mr-5" href="https://github.com/hadiprasetiyo?tab=overview&from=2022-12-01&to=2022-12-26" target="_blank"><i class="fab fa-github"></i></a>
+        </div>
+
+      </div>
+      <div class="col-md-7 col-md-push-1">
+        <div class="row">
+          <div
+            class="col-md-10 col-md-offset-1 col-md-pull-1 animate-box"
+            data-animate-effect="fadeInRight"
+          >
+            <form action="">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Name" />
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Email" />
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Subject" />
+              </div>
+              <div class="form-group">
+                <textarea
+                  name=""
+                  id="message"
+                  cols="30"
+                  rows="7"
+                  class="form-control"
+                  placeholder="Message"
+                ></textarea>
+              </div>
+              <div class="form-group">
+                <input
+                  type="submit"
+                  class="btn btn-primary btn-send-message"
+                  value="Send Message"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
